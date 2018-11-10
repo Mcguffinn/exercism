83ffecd3cdbc110dabcc6907db5c66faded5692a
Edwin <deronviledwin@gmail.com>
2018-11-10 04:35:48 -0500

    another cleaned version

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -1,32 +1,25 @@
 class Phone(object):
     def __init__(self, phone_number):
-        # invalid = set()
-        self.holder = phone_number
-        raw = self.cleaner(self.holder)
-        if raw[0] == '1':
-            self.number = raw[1:]
-        else:
-            self.number = raw
-        
+        self.number = self.cleaner(phone_number)
         self.area_code = self.number[0:3]
         self.exchange = self.number[3:6]
         self.rest = self.number[6:]
+        
+        if self.number[0] in '0' or self.number[3] in '10':
+            raise ValueError('Invalid1')
+        if self.area_code[0] in '01':
+            raise ValueError('wrong')
+        if len(self.number) <= 9 or len(self.number) > 11 or len(self.number) == 11 and self.number[0] in '2':
+            raise ValueError('nope')
+        print(self.number, self.number[0])
+        # print(self.area_code)
 
-        if len(self.number) <= 9 or len(self.number) > 11:
-            raise ValueError('Invalid num')
-        if self.number[0] != '1' and len(self.number) >= 11:
-            raise ValueError('Invalid num')
-        if self.exchange[0] in ('1','0'):
-            raise ValueError('invalid num')
-        if self.area_code[0] in ('1','0'):
-            raise ValueError('Invalid num') 
-
-        print(raw[1:])
-        print(self.area_code)
-    
-    def cleaner(self, anything):
-        clean = ''.join(c for c in self.holder if c.isdigit())
-        return clean
+    def cleaner(self, phone_number):
+        clean = ''.join(c for c in phone_number if c.isdigit())
+        if clean[0] == '1':
+            return clean[1:]
+        else:
+            return clean
 
     def pretty(self,):
         outcome = '({}) {}-{}'.format(self.area_code, self.exchange,self.rest)
