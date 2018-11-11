50088c98130df9fa5ca309e0ab21c77a99e82a03
Edwin <deronviledwin@gmail.com>
2018-11-11 04:10:24 -0500

    finished

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -3,27 +3,21 @@ class HighScores(object):
         self.scores = scores
 
     def top(self):
-        pass
-        rank = self.scores.sort(reverse = True)
-        return self.scores[0:3]
+        rank = sorted(self.scores, reverse = True)
+        return rank[0:3]
+
+    def large(self):
+        rank = self.scores.sort()
+        return rank
     
-    def report(self,):
-        a = self.highest(self.scores)
-        b = self.latest(self.scores)
-        w = [a - b for (a, b) in zip(a, b)]
-        print(a,b)
-        # if new > item1:
-        #     rep = "Your latest score was {}. That's your personal best!".format(self.highest())
-        #     print(rep)
-        # else:
-        #     rep = "Your latest score was {}. \
-        #     That's {} short of your personal best!".format(self.latest(),self.highest())
-        # return rep
+    def report(self):
+        short = (" {} short of".format(self.highest() - self.latest()),"")[self.latest() == self.highest()]
+        return "Your latest score was {}. That's{} your personal best!".format(self.latest(), short)
     
-    def highest(self,scores):
+    def highest(self,):
         high = self.top()[0]
         return high
     
-    def latest(self,scores):
+    def latest(self,):
         new = self.scores[-1]
         return new
